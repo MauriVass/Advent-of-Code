0e8b5b696f73e16346ed8c8bfecaeff89ecbca1c
MauriVass <maurizio140333@gmail.com>
2022-12-06 14:57:48 +0100

    Solution challenge 6 pt 2

--- Day 6/script.py
+++ Day 6/script.py
@@ -1,18 +1,18 @@
 f = open('input.txt')
 # f = open('example.txt')
-pt1 = True
+pt1 = False
 
 start_of_packet_marker_index = 0
 characters = []
-length_starter = 4
+length_starter = 4 if pt1 else 14
 for index, character in enumerate(f.read()):
     if(index>=length_starter):
         characters.pop(0)
     characters.append(character)
-    if(index>2 and len(set(characters))==length_starter):
+    if(len(set(characters))==length_starter):
         start_of_packet_marker_index = index+1
         break
 
-print(start_of_packet_marker_index) #pt1 1920
+print(start_of_packet_marker_index) #pt1 1920 pt2 2334
 f.close()
 
