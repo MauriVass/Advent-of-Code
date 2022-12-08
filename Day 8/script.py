b532119563423b9bf66af4b760030019fb76229a
MauriVass <maurizio140333@gmail.com>
2022-12-08 22:40:33 +0100

    Solution challenge 7 pt 1

--- Day 8/script.py
+++ Day 8/script.py
@@ -3,26 +3,25 @@ class Tree:
         self.row = row
         self.col = col
         self.height = height
-        if(height==9):
-            val = True
-        else:
-            val = False
+        val = 1
         self.visible_from_top = val
         self.visible_from_right = val
         self.visible_from_bottom = val
         self.visible_from_left = val
 
     def isVisible(self):
-        visible = self.visible_from_top or self.visible_from_right or self.visible_from_bottom or self.visible_from_left
+        visible = self.visible_from_top==0 or \
+                  self.visible_from_right==0 or \
+                  self.visible_from_bottom==0 or \
+                  self.visible_from_left==0
         return visible
 
-# f = open('input.txt')
-f = open('example.txt')
+f = open('input.txt')
+# f = open('example.txt')
 pt1 = False
 
 forest = []
 
-visible = 0
 row = 0
 for line in f.readlines():
     tmp = []
@@ -35,7 +34,6 @@ for line in f.readlines():
 f.close()
 
 visible = 4 * (row-1)
-print(visible)
 for r in range(1,row-1):
     for c in range(1,row-1):
         tree = forest[r][c]
@@ -44,9 +42,9 @@ for r in range(1,row-1):
             for top in range(0,r):
                 h = forest[top][c].height
                 if(h >= tree.height):
-                    tree.visible_from_top = False
-            if(tree.visible_from_top==False):
-                tree.visible_from_top = True
+                    tree.visible_from_top += 1
+            if(tree.visible_from_top==1):
+                tree.visible_from_top = 0
                 visible+=1
 
         if(tree.isVisible()==False):
@@ -54,32 +52,31 @@ for r in range(1,row-1):
             for right in range(c+1,row):
                 h = forest[r][right].height
                 if(h >= tree.height):
-                    tree.visible_from_right = True
-            if(tree.visible_from_right==False):
-                tree.visible_from_right = True
-                visible+=1
+                    tree.visible_from_right += 1
+            if (tree.visible_from_right == 1):
+                tree.visible_from_right = 0
+                visible += 1
 
         if(tree.isVisible()==False):
             #from bottom
             for bottom in range(r+1,row):
                 h = forest[bottom][c].height
                 if(h >= tree.height):
-                    tree.visible_from_bottom = False
-            if(tree.visible_from_bottom==False):
-                tree.visible_from_bottom = True
-                visible+=1
+                    tree.visible_from_bottom += 1
+            if (tree.visible_from_bottom == 1):
+                tree.visible_from_bottom = 0
+                visible += 1
 
         if(tree.isVisible()==False):
             #from left
             for left in range(0,c):
-                h = forest[r][bottom].height
+                h = forest[r][left].height
                 if(h >= tree.height):
-                    tree.visible_from_left = False
-            if(tree.visible_from_left==False):
-                tree.visible_from_left = True
-                visible+=1
-
-print(visible)
+                    tree.visible_from_left += 1
+            if (tree.visible_from_left == 1):
+                tree.visible_from_left = 0
+                visible += 1
+print(visible) #pt1 1711
 
 
 
