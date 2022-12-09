c03194904fedc6b5799085fa429a510bac0eb03f
MauriVass <maurizio140333@gmail.com>
2022-12-09 16:08:58 +0100

    Solution challenge 9 pt 1

--- Day 9/script.py
+++ Day 9/script.py
@@ -8,7 +8,7 @@ def calculateDistance(head,tail):
     distance = math.fabs(head[0]-tail[0]) + math.fabs(head[1]-tail[1])
     return distance
 def changeTailPos(head,tail):
-    distance = calculateDistance(head,tail)
+    distance = calculateDistance(head, tail)
     move = False
     if(tail[0] != head[0] and tail[1] != head[1]):
         if(distance>2):
@@ -25,46 +25,22 @@ head_pos = [0,0]
 tail_pos = [0,0]
 visited_position = []
 visited_position.append(head_pos)
+commands = {'U':[0, 1], 'R':[1, 0], 'D':[0, -1], 'L':[-1, 0]}
+
 for line in f.readlines():
     line = line[:-1].split(' ')
     command = line[0]
     distance = int(line[1])
 
-    #Up
-    if(command=='U'):
-        for d in range(1, distance+1):
-            head_pos = changePosition(head_pos, (0, 1))
-            if(changeTailPos(head_pos,tail_pos)):
-                tail_pos = changePosition(head_pos, (0, -1))
-                if(tail_pos not in visited_position):
-                    visited_position.append(tail_pos)
+    for d in range(1, distance+1):
+        vector = commands[command]
 
-    #Right
-    elif(command=='R'):
-        for d in range(1, distance+1):
-            head_pos = changePosition(head_pos, (1, 0))
-            if(changeTailPos(head_pos,tail_pos)):
-                tail_pos = changePosition(head_pos, (-1, 0))
-                if(tail_pos not in visited_position):
-                    visited_position.append(tail_pos)
+        head_pos = changePosition(head_pos, vector)
+        if(changeTailPos(head_pos,tail_pos)):
+            tail_pos = changePosition(head_pos, [-vector[0],-vector[1]])
 
-    #Down
-    elif (command == 'D'):
-        for d in range(1, distance+1):
-            head_pos = changePosition(head_pos, (0, -1))
-            if(changeTailPos(head_pos,tail_pos)):
-                tail_pos = changePosition(head_pos, (0, 1))
-                if(tail_pos not in visited_position):
-                    visited_position.append(tail_pos)
-
-    #Left
-    elif (command == 'L'):
-        for d in range(1, distance+1):
-            head_pos = changePosition(head_pos, (-1, 0))
-            if(changeTailPos(head_pos,tail_pos)):
-                tail_pos = changePosition(head_pos, (1, 0))
-                if (tail_pos not in visited_position):
-                    visited_position.append(tail_pos)
+        if (tail_pos not in visited_position):
+            visited_position.append(tail_pos)
 print(len(visited_position)) #pt1 6212
 f.close()
 
